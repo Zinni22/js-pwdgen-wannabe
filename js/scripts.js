f4d496320eca65bbe6c4fd6c4d3cfee748b4bee0
Zineb Atlassi <atlassizineb@gmail.com>
2022-12-19 10:14:58 +0100

    Correzione aggiunta let per semplificare

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,19 @@
 // chiedo il nome all'utente
 const nome = prompt("Inserisci il tuo nome");
-console.log("l'utente si chiama", nome)
+console.log("l'utente si chiama", nome);
 
 // chiedo il cognome all'utente
 const surname = prompt("Inserisci il tuo cognome");
-console.log("il cognome dell'utente è", surname)
+console.log("il cognome dell'utente è", surname);
 
 // chiedo il colore preferito all'utente
 const color = prompt("Inserisci il tuo colore preferito");
-console.log("il colore preferito dell'utente è", color)
+console.log("il colore preferito dell'utente è", color);
+
+
+//Unisco i dati per creare la password
+let password = nome+ ' ' +surname+ ' ' +color+ ' '+22;
+console.log = (password)
 
 //Compare tutto nel foglio HTML
-document.getElementById("my-id").innerHTML = (nome+ ' ' +surname+ ' ' +color+ ' '+22)
+document.getElementById("my-id").innerHTML = password;
